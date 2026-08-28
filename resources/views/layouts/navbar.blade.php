431227dfccc181344056c15280f019e9e9e71bf3
valtzy220 <nauvalfauzi99@gmail.com>
2026-08-28 09:41:39 +0700

    bug active

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,16 +2,16 @@
     <div class="navbar-container">
        
 
-        <ul class="navbar-menu">
-            <li><a href="{{ Route('beranda') }}" class="navbar-link {{ request()->is('/') ? 'active' : '' }}">Beranda</a></li>
-            <li><a href="{{ route('profile') }}" class="navbar-link {{ request()->is('profile') ? 'active' : '' }}">Profile</a></li>
-            <li><a href="{{ route('data-guru') }}" class="navbar-link {{ request()->is('data-guru') ? 'active' : '' }}">Guru</a></li>
-            <li><a href="{{ route('mata-pelajaran') }}" class="navbar-link {{ request()->is('mapel') ? 'active' : '' }}">Mata Pelajaran</a></li>
-            <li><a href="{{ route('contact') }}" class="navbar-link {{ request()->is('contact') ? 'active' : '' }}">Contact</a></li>
-            <li><a href="{{ route('fasilitas') }}" class="navbar-link {{ request()->is('fasilitas') ? 'active' : '' }}">fasilitas</a></li>
-            <li><a href="{{ route('prestasi') }}" class="navbar-link {{ request()->is('prestasi') ? 'active' :''}}">prestasi</a></li>
-            <li><a href="{{ route('gallery') }}" class="navbar-link {{ request()->is('gallery') ? 'active' : '' }}">Gallery</a></li>
-        </ul>
+    <ul class="navbar-menu">
+    <li><a href="{{ route('beranda') }}" class="navbar-link {{ request()->routeIs('beranda') ? 'active' : '' }}">Beranda</a></li>
+    <li><a href="{{ route('profile') }}" class="navbar-link {{ request()->routeIs('profile') ? 'active' : '' }}">Profile</a></li>
+    <li><a href="{{ route('data-guru') }}" class="navbar-link {{ request()->routeIs('data-guru*') ? 'active' : '' }}">Guru</a></li>
+    <li><a href="{{ route('mata-pelajaran') }}" class="navbar-link {{ request()->routeIs('mata-pelajaran*') ? 'active' : '' }}">Mata Pelajaran</a></li>
+    <li><a href="{{ route('contact') }}" class="navbar-link {{ request()->routeIs('contact') ? 'active' : '' }}">Contact</a></li>
+    <li><a href="{{ route('fasilitas') }}" class="navbar-link {{ request()->routeIs('fasilitas*') ? 'active' : '' }}">Fasilitas</a></li>
+    <li><a href="{{ route('prestasi') }}" class="navbar-link {{ request()->routeIs('prestasi*') ? 'active' : '' }}">Prestasi</a></li>
+    <li><a href="{{ route('gallery') }}" class="navbar-link {{ request()->routeIs('gallery*') ? 'active' : '' }}">Gallery</a></li>
+</ul>
 
         <button class="navbar-toggle" id="navbar-toggle">
             <span></span>
